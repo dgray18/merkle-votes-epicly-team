06979fdfd6978955697812bc0fa0791bb216c2bb
dgray18 <31670425+dgray18@users.noreply.github.com>
2017-11-30 21:50:31 -0700

    Update vote.cpp

--- vote.cpp
+++ vote.cpp
@@ -7,16 +7,23 @@ using namespace std;
 
 void readFile(string fileName)
 {
-	string line;
-	ifstream myfile(fileName);
-	if (myfile.is_open())
-	{
-		myfile.close();
-	}
-	else
-	{
-		cout << "ERROR: Unable to open file" << endl;
-	}
+std::fstream in("mv_test.txt");
+
+if (!in) {
+    cout << "unable to open file";
+    return 0;
+}
+
+char test[255];
+string line;
+char delimeter(':');
+while(in){
+    in.getline(test, 255, delimeter);
+
+    cout << test << endl;
+}
+in.close();
+return 0;
 }
 
 
